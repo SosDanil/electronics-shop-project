c7731a8d2efc16e556203ef7a9ff1b130b8c8ad7
Danil Klimenko <sos.danil@yandex.ru>
2024-01-24 19:45:09 +0500

    add fixture in tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,19 +1,26 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
 from src.item import Item
 from config import PATH_TO_CSV
 
 
-item1 = Item("Samsung M3", 10000.0, 5)  # тестовый экземпляр
+# item1 = Item("Samsung M3", 10000.0, 5)  # тестовый экземпляр
 
 
-def test_calculate_total_price():
-    assert item1.calculate_total_price() == 50000
+@pytest.fixture
+def test_item1():
+    return Item("Samsung M3", 10000.0, 5)
 
 
-def test_apply_discount():
-    item1.pay_rate = 0.5
-    item1.apply_discount()
-    assert item1.price == 5000
+def test_calculate_total_price(test_item1):
+    assert test_item1.calculate_total_price() == 50000
+
+
+def test_apply_discount(test_item1):
+    test_item1.pay_rate = 0.5
+    test_item1.apply_discount()
+    assert test_item1.price == 5000
 
 
 def test_string_to_number():
@@ -32,6 +39,6 @@ def test_instantiate_from_csv():
     assert item2.quantity == 3
 
 
-def test_repr():
-    assert repr(item1) == "Item('Samsung M3', 10000.0, 5)"
-    assert str(item1) == "Samsung M3"
+def test_repr(test_item1):
+    assert repr(test_item1) == "Item('Samsung M3', 10000.0, 5)"
+    assert str(test_item1) == "Samsung M3"
